60e8a4ffef7d4c46045ea28ef638a030cc2b003a
GabrielGardev <gabriel.gurdev@gmail.com>
2020-11-02 10:26:20 +0200

    refactoring

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -39,21 +39,6 @@ public class AllocationServiceImpl implements AllocationService {
         return totalOrdered.get() == totalAllocated.get();
     }
 
-    @Override
-    public void deallocateOrder(BeerOrderDto beerOrderDto) {
-        beerOrderDto.getBeerOrderLines().forEach(beerOrderLineDto -> {
-            BeerInventory beerInventory = BeerInventory.builder()
-                    .beerId(beerOrderLineDto.getBeerId())
-                    .upc(beerOrderLineDto.getUpc())
-                    .quantityOnHand(beerOrderLineDto.getQuantityAllocated())
-                    .build();
-
-            BeerInventory savedInventory = beerInventoryRepository.save(beerInventory);
-
-            log.debug("Saved Inventory for beer upc: " + savedInventory.getUpc() + " inventory id: " + savedInventory.getId());
-        });
-    }
-
     private void allocateBeerOrderLine(BeerOrderLineDto beerOrderLine) {
         List<BeerInventory> beerInventoryList = beerInventoryRepository.findAllByUpc(beerOrderLine.getUpc());
 
@@ -79,5 +64,20 @@ public class AllocationServiceImpl implements AllocationService {
             }
         });
 
+    }
+
+    @Override
+    public void deallocateOrder(BeerOrderDto beerOrderDto) {
+        beerOrderDto.getBeerOrderLines().forEach(beerOrderLineDto -> {
+            BeerInventory beerInventory = BeerInventory.builder()
+                    .beerId(beerOrderLineDto.getBeerId())
+                    .upc(beerOrderLineDto.getUpc())
+                    .quantityOnHand(beerOrderLineDto.getQuantityAllocated())
+                    .build();
+
+            BeerInventory savedInventory = beerInventoryRepository.save(beerInventory);
+
+            log.debug("Saved Inventory for beer upc: " + savedInventory.getUpc() + " inventory id: " + savedInventory.getId());
+        });
     }
 }
